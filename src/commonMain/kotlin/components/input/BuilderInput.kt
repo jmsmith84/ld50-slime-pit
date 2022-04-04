f7719e89eea532c166deafa00107abcd828d100d
Joel Smith <jxmxsx@gmail.com>
2022-04-05 03:04:04 +0900

    Allow build action button to be held down, more enjoyable if you can build in a line

--- src/commonMain/kotlin/components/input/BuilderInput.kt
+++ src/commonMain/kotlin/components/input/BuilderInput.kt
@@ -1,9 +1,10 @@
 package components.input
 
+import com.soywiz.klock.TimeSpan
 import com.soywiz.klock.milliseconds
 import com.soywiz.korau.sound.Sound
 import com.soywiz.korev.Key
-import com.soywiz.korge.component.Component
+import com.soywiz.korge.component.UpdateComponent
 import com.soywiz.korge.input.keys
 import com.soywiz.korge.view.Sprite
 import com.soywiz.korge.view.SpriteAnimation
@@ -26,13 +27,15 @@ class BuilderInput(
     val key: Key,
     private val levelManager: LevelManager,
     private val soundManager: SoundManager,
-    wallBuildingAnimation: SpriteAnimation,
+    private val wallBuildingAnimation: SpriteAnimation,
     private val playerBuildingAnimation: SpriteAnimation? = null,
     private val wallBuiltSound: Sound? = null
-) : Component {
+) : UpdateComponent {
     private val buildTime: Duration = 2.38.seconds
     private var wallSprite: Sprite
     var builderTimer: EventTimer = EventTimer(view, buildTime) {}
+    var buildingMode = false
+    var buildingDirection: MoveDirection = MoveDirection.UP
 
     init {
         view.addComponent(builderTimer)
@@ -40,48 +43,18 @@ class BuilderInput(
 
         view.keys.justDown(key) {
             Log().debug { "Z down" }
+            buildingMode = true
+            buildingDirection = view.getFacing()
 
             if (!builderTimer.isRunning()) {
-                val playerPosition = view.pos.copy()
-                val bounds = view.getBounds()
-
-                when (view.getFacing()) {
-                    MoveDirection.UP -> playerPosition.add(bounds.centerX, bounds.top - (bounds.height / 2))
-                    MoveDirection.DOWN -> playerPosition.add(bounds.centerX, bounds.bottom + (bounds.height / 2))
-                    MoveDirection.LEFT -> playerPosition.add(bounds.left - (bounds.width / 2), bounds.centerY)
-                    MoveDirection.RIGHT -> playerPosition.add(bounds.right + (bounds.width / 2), bounds.centerY)
-                    else -> {}
-                }
-
-                val tileXY = levelManager.globalXYToTileXY(playerPosition)
-                if (!levelManager.isTileEmpty(tileXY)) return@justDown
-
-                view.canMove = false
-                wallSprite = Sprite(wallBuildingAnimation, smoothing = false) /* Have to create a new sprite
-                    every time due to a Sprite bug where remaining frame time is never reset */
-                wallSprite.position(levelManager.tileXYToGlobalXY(tileXY))
-                wallSprite.addTo(levelManager.getMapView())
-                wallSprite.playAnimation(spriteDisplayTime = 800.0.milliseconds / view.speedModifier, startFrame = 0)
-
-                builderTimer.newCallback {
-                    stopBuilding()
-                    if (!levelManager.isTileEmpty(tileXY)) return@newCallback
-                    levelManager.setTileIdAt(tileXY.x.toInt(), tileXY.y.toInt(), WallTileStatus.WOOD_NORMAL.id)
-
-                    if (wallBuiltSound !== null) soundManager.asyncPlaySound(wallBuiltSound)
-                }
-                builderTimer.setLength(buildTime / view.speedModifier)
-                builderTimer.restart()
+                tryBuilding()
             }
-            if (playerBuildingAnimation !== null) view.getSprite().playAnimationLooped(
-                playerBuildingAnimation,
-                150.milliseconds
-            )
         }
         view.keys.up {
             if (it.key == key) {
                 Log().debug { "Z up" }
                 stopBuilding()
+                buildingMode = false
             }
         }
     }
@@ -92,5 +65,49 @@ class BuilderInput(
         wallSprite.stopAnimation()
         wallSprite.removeFromParent()
         view.resetSpriteImage()
+    }
+
+    private fun tryBuilding() {
+        val playerPosition = view.pos.copy()
+        val bounds = view.getBounds()
+
+        when (buildingDirection) {
+            MoveDirection.UP -> playerPosition.add(bounds.centerX, bounds.top - (bounds.height / 2))
+            MoveDirection.DOWN -> playerPosition.add(bounds.centerX, bounds.bottom + (bounds.height / 2))
+            MoveDirection.LEFT -> playerPosition.add(bounds.left - (bounds.width / 2), bounds.centerY)
+            MoveDirection.RIGHT -> playerPosition.add(bounds.right + (bounds.width / 2), bounds.centerY)
+            else -> {}
+        }
+
+        val tileXY = levelManager.globalXYToTileXY(playerPosition)
+        if (!levelManager.isTileEmpty(tileXY)) return
+
+        view.canMove = false
+        wallSprite = Sprite(wallBuildingAnimation, smoothing = false) /* Have to create a new sprite
+                    every time due to a Sprite bug where remaining frame time is never reset */
+        wallSprite.position(levelManager.tileXYToGlobalXY(tileXY))
+        wallSprite.addTo(levelManager.getMapView())
+        wallSprite.playAnimation(spriteDisplayTime = 800.0.milliseconds / view.speedModifier, startFrame = 0)
+
+        builderTimer.newCallback {
+            stopBuilding()
+            if (!levelManager.isTileEmpty(tileXY)) return@newCallback
+            levelManager.setTileIdAt(tileXY.x.toInt(), tileXY.y.toInt(), WallTileStatus.WOOD_NORMAL.id)
+
+            if (wallBuiltSound !== null) soundManager.asyncPlaySound(wallBuiltSound)
+        }
+        builderTimer.setLength(buildTime / view.speedModifier)
+        builderTimer.restart()
+
+        if (playerBuildingAnimation !== null) view.getSprite().playAnimationLooped(
+            playerBuildingAnimation,
+            150.milliseconds
+        )
+    }
+
+    override fun update(dt: TimeSpan) {
+        if (buildingMode && !builderTimer.isRunning()) {
+            tryBuilding()
+        }
     }
 }
